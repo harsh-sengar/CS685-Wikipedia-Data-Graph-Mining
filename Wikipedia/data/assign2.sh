774d1a0beae221a31dcb901a824ddcf4330f339e
Harshvardhan Pratap Singh <30585848+harsh-sengar@users.noreply.github.com>
2022-08-25 15:33:20 +0530

    Add files via upload

---
+++ Wikipedia/data/assign2.sh
@@ -0,0 +1,23 @@
+echo "question 1 is running"
+python Q1.py
+echo "question 2 is running"
+python Q2.py
+echo "question 3 is running"
+python Q3.py
+echo "question 4 is running"
+python Q4.py
+echo "question 5 is running"
+python Q5.py
+echo "question 6 is running"
+python Q6.py
+echo "question 7 is running"
+python Q7.py
+echo "question 8 is running"
+python Q8.py
+echo "question 9 is running"
+python Q9.py
+echo "question 10 is running"
+python Q10.py
+echo "question 11 is running"
+python Q11.py
+echo "execution successful."
